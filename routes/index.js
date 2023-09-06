d8e8a79d63fbdcad69e1845952cbfa6de3eb0e26
eddy ruiz <eddyvaldo@live.com.mx>
2023-09-06 16:51:43 +0000

    ⭐Enhance:app add author route to users router

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+// un GET / se ejecutara esta
+//registro de middlewares de app,tenemos enrutadores 
+
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'ITGAM',author: "Eddy" });
 });
 
 module.exports = router;
